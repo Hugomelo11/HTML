b2fc07343eb0e3ac84c9921da00dc49d28cbcb5f
Hugomelo <salles.hugo@gmail.com>
2022-08-01 15:10:54 -0400

    adding more lists to the api

--- students.js
+++ students.js
@@ -1,17 +1,25 @@
-import express from "express"
-import cors from "cors"
-const app = express()
-const port = 4001
+import express from "express";
+import cors from "cors";
+const app = express();
+const port = 4001;
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
+let students = ["bob", "samantha", "grace", "jim", "lily"];
+
 app.get("/", (req, res) => {
-    res.send("hello world")
+  let output = "<html><body><ul>"; // starts
+
+  // output = output +  "hello there html"  // middle
+  for (let i = 0; i < students.length; i++) {
+    output = output + '<li><input type="checkbox">' + students[i] + "</li>";
+  }
+  output = output + "</ul></body></html>"; // end
+
+  res.send(output);
 });
 
-
-
 app.listen(port, () => {
-    console.log("i was needed")
+  console.log("localhost:4000");
 });
